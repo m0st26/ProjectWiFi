a2fe868f96dfa2e1e2ae47b29acd33e3c75354be
Ilya <51093023+m0st26@users.noreply.github.com>
2020-05-16 18:19:19 +0300

    lab5

---
+++ ProjectParkWiFi.WebService/InfrastructureServices/Gateways/Database/ParkWiFiContext.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ParkWiFi.DomainObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParkWiFi.InfrastructureServices.Gateways.Database
+{
+    public class ParkWiFiContext : DbContext
+    {
+        public DbSet<DomainObjects.ParkWiFi> ParkWiFis { get; set; }
+
+
+        public ParkWiFiContext(DbContextOptions options)
+            : base(options)
+        { }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<DomainObjects.ParkWiFi>().HasData(
+              new DomainObjects.ParkWiFi()
+              {
+                  Id = 1L,
+                  Name = "Точка доступа №337",
+                  status = "действует",
+                  ParkName = "Государственное автономное учреждение культуры города Москвы «Государственный историко-архитектурный и природно-ландшафтный музей-заповедник Кузьминки-Люблино, Парк Кузбминки",
+                  wifiName = "Moscow_WiFi_Free",
+              }
+           );
+
+            //FillTestData(modelBuilder);
+        }
+        //private void FillTestData(ModelBuilder modelBuilder)
+        //{
+           
+
+        //    modelBuilder.Entity<ManageOrg>().HasData(
+        //       new ManageOrg()
+        //       {
+        //           Id = 1L,
+        //           Name = "Товарищество собственников жилья «Рождественский бульвар дом 5-7 » 7702560790 2015",
+        //           Year = 2015,
+        //           Prescriptions = "1",
+        //           Contraventions = 3,
+        //           Protocols = 0,
+        //           Events = 0
+        //       }
+        //    );
+        //}
+    }
+}
---
+++ ProjectParkWiFi.WebService/InfrastructureServices/Gateways/Database/ParkWiFiEFSqliteGateway.cs
@@ -0,0 +1,49 @@
+﻿using ParkWiFi.DomainObjects;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Linq.Expressions;
+using ParkWiFi.ApplicationServices.Ports.Gateways.Database;
+
+namespace ParkWiFi.InfrastructureServices.Gateways.Database
+{
+    public class ParkWiFiEFSqliteGateway : IParkWiFiDatabaseGateway
+    {
+        private readonly ParkWiFiContext _parkwifiContext;
+
+        public ParkWiFiEFSqliteGateway(ParkWiFiContext parkwifiContext)
+            => _parkwifiContext = parkwifiContext;
+
+        public async Task<DomainObjects.ParkWiFi> GetParkWiFi(long id)
+           => await _parkwifiContext.ParkWiFis.FirstOrDefaultAsync();
+
+        public async Task<IEnumerable<DomainObjects.ParkWiFi>> GetAllParkWiFi()
+            => await _parkwifiContext.ParkWiFis.ToListAsync();
+          
+        public async Task<IEnumerable<DomainObjects.ParkWiFi>> QueryParkWiFi(Expression<Func<DomainObjects.ParkWiFi, bool>> filter)
+            => await _parkwifiContext.ParkWiFis.Where(filter).ToListAsync();
+
+        public async Task AddParkWiFi(DomainObjects.ParkWiFi parkwifi)
+        {
+            _parkwifiContext.ParkWiFis.Add(parkwifi);
+            await _parkwifiContext.SaveChangesAsync();
+        }
+
+        public async Task UpdateParkWiFi(DomainObjects.ParkWiFi parkwifi)
+        {
+            _parkwifiContext.Entry(parkwifi).State = EntityState.Modified;
+            await _parkwifiContext.SaveChangesAsync();
+        }
+
+        public async Task RemoveParkWiFi(DomainObjects.ParkWiFi parkwifi)
+        {
+            _parkwifiContext.ParkWiFis.Remove(parkwifi);
+            await _parkwifiContext.SaveChangesAsync();
+        }
+
+
+       
+    }
+}
