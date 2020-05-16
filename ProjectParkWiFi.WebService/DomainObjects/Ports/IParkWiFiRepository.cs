5ee948db23a287c495279d6e0c64266c044ca6b0
Ilya <51093023+m0st26@users.noreply.github.com>
2020-05-16 18:08:39 +0300

    lab1

---
+++ ProjectParkWiFi.WebService/DomainObjects/Ports/IParkWiFiRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq.Expressions;
+
+namespace ParkWiFi.DomainObjects.Ports
+{
+    public interface IReadOnlyParkWiFiRepository
+    {
+        Task<ParkWiFi> GetParkWiFi(long id);
+
+        Task<IEnumerable<ParkWiFi>> GetAllParkWiFi();
+
+        Task<IEnumerable<ParkWiFi>> QueryParkWiFi(ICriteria<ParkWiFi> criteria);
+
+    }
+
+    public interface IParkWiFiRepository
+    {
+        Task AddParkWiFi(ParkWiFi parkwifi);
+
+        Task RemoveParkWiFi(ParkWiFi parkwifi);
+
+        Task UpdateParkWiFi(ParkWiFi parkwifi);
+    }
+}
